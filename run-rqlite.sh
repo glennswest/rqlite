e6f376b7c1048de5e5cf7f1266dca0862182959b
gwest <glennswest@neuralcloudcomputing.com>
2025-03-28 13:32:52 -0500

    Fix path for data

--- run-rqlite.sh
+++ run-rqlite.sh
@@ -1,6 +1,7 @@
 podman stop rqlite
 podman rm rqlite
 podman volume rm rqlite
+mkdir -p /volumes/rqlite
 podman volume create rqlite -o device=/volumes/rqlite -o=o=bind
 
-podman run -d -v rqlite:/data:rw,Z --name rqlite -p 4001:4001 docker.io/rqlite/rqlite:latest
+podman run -d -v rqlite:/rqlite:rw,Z --name rqlite -p 4001:4001 docker.io/rqlite/rqlite:latest
