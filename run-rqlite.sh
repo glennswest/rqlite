71fde9c7fd50010d30ee5c23b8f0ac74026b303a
gwest <gwest@redhat.com>
2025-04-17 16:13:26 -0500

    Change to use local copy to allow running on arm
    	modified:   run-rqlite.sh

--- run-rqlite.sh
+++ run-rqlite.sh
@@ -1,7 +1,8 @@
 podman stop rqlite
 podman rm rqlite
 podman volume rm rqlite
-mkdir -p /volumes/rqlite
-podman volume create rqlite -o device=/volumes/rqlite -o=o=bind
+podman volume create rqlite
+# mkdir -p /volumes/rqlite
+# podman volume create rqlite -o device=/volumes/rqlite -o=o=bind
 
-podman run -d -v rqlite:/rqlite:rw,Z --name rqlite -p 4001:4001 docker.io/rqlite/rqlite:latest
+podman run -d -v rqlite:/rqlite:rw,Z --name rqlite -p 4001:4001 localhost/rqlite  
